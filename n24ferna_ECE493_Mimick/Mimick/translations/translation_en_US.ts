57f6660dc1b328efde18295f290769aad6d0e259
Neil Fernandes <66129484+Fatiepie@users.noreply.github.com>
2024-03-05 12:14:17 -0500

    added files for mimick

---
+++ n24ferna_ECE493_Mimick/Mimick/translations/translation_en_US.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello, My name is NAO and welcome to the experiment. Please have a seat!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello, My name is NAO and welcome to the experiment. Please have a seat!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <source>Today I will be talking about the great works of Shakespeare with some non verbal communication cues. See if you can catch them EHE</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Today I will be talking about the great works of Shakespeare with some non verbal communication cues. See if you can catch them EHE</translation>
+        </message>
+        <message>
+            <source>Hello, My name is NAO and welcome to the experiment. Please have a seat!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello, My name is NAO and welcome to the experiment. Please have a seat!</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Testing one two threee fourfuve and six</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Testing one two threee fourfuve and six</translation>
+        </message>
+    </context>
+</TS>
